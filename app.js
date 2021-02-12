9a7530080699bb9e7a59f34901d62644df765f7c
Cheng <Wellman.Cheng@calpers.ca.gov>
2021-02-12 09:29:39 -0800

    change message

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-
+console.log(`Hello World`);
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
