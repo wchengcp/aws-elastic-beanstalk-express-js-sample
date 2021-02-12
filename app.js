5d7ab0fc1729ccaa52dd7ebe80585b649fccb9be
wchengcp <78870959+wchengcp@users.noreply.github.com>
2021-02-12 11:45:10 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 console.log(`Hello World`);
+app.get('/', (req, res) => res.send('This is a test!'));
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
